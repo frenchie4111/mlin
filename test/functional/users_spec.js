da964580637f16ff078e9152d9ba9870fcc0f733
Mike Lyons <mdl0394@gmail.com>
2014-07-06 15:25:52 -0700

    Added user_spec tests

--- test/functional/users_spec.js
+++ test/functional/users_spec.js
@@ -5,8 +5,14 @@ var request = require( "supertest" ),
 describe( "User Routing", function() {
     var url = "http://localhost:1337";
 
-    beforeEach( function( done ) {
+    before( function( done ) {
         server.start( { port: 1337, enviroment: "test", print: false }, function() {
+            done();
+        } );
+    } );
+
+    after( function( done ) {
+        server.close( function() {
             done();
         } );
     } );
@@ -25,10 +31,55 @@ describe( "User Routing", function() {
                 .expect( 200 )
                 .end( function( err, body ) {
                     if( err ) { throw err; };
+
+
+
                     done();
                 } );
         } );
     } );
 
+    describe( "User Login", function() {
+        var profile = {
+            username: "test2",
+            password: "test2",
+            email: "test2@test.com"
+        }
+
+        it( "Should create account and sign in with it", function( done ) {
+            request( url )
+                .post( "/register" )
+                .send( profile )
+                .expect( 200 )
+                .end( function( err, res ) {
+                    if( err ) { throw err; };
+
+                    assert.equal( profile.username, res.body.username );
+                    assert.equal( profile.email, res.body.email );
+
+                    request( url )
+                        .post( "/login" )
+                        .send( profile )
+                        .expect( 200 )
+                        .end( function( err, res ) {
+
+                            assert( res.body.token != null );
+
+                            request( url )
+                                .get( "/me" )
+                                .set( "mlin-authentication", res.body.token )
+                                .expect( 200 )
+                                .end( function( err, res ) {
+
+                                    assert( res.body );
+                                    assert.equal( res.body.username, profile.username );
+                                    assert.equal( res.body.email, profile.email );
+
+                                    done();
+                                } );
+                        } );
+                } );
+        } );
+    } );
 
 } );
